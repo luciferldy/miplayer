d977ec28a35b2dab34b5ba091bd15e142e673c34
MaybeMercy <2460572865@qq.com>
2015-05-17 23:03:03 +0800

    loadinglist task get albums and get songs done

--- app/src/main/java/com/main/maybe/miplayer/task/LoadingListTask.java
+++ app/src/main/java/com/main/maybe/miplayer/task/LoadingListTask.java
@@ -1,8 +1,10 @@
 package com.main.maybe.miplayer.task;
 
+import android.content.ContentResolver;
 import android.content.Context;
+import android.database.Cursor;
 import android.os.AsyncTask;
-import android.os.Handler;
+import android.provider.MediaStore;
 
 import com.main.maybe.miplayer.db.HandleDB;
 import com.main.maybe.miplayer.music.MusicScanner;
@@ -17,6 +19,17 @@ public class LoadingListTask extends AsyncTask<Void, Void, Void> {
 
     private int type = 0; // 0 song, 1 artist, 2 album
     private Context context;
+    private final String songId = "songId";
+    private final String songName = "songName";
+    private final String artistName = "artistName";
+    private final String albumName = "albumName";
+    private final String path = "path";
+    private final String duration = "duration";
+
+    private final String artistId = "artistId";
+    private final String songNumber = "songNumber";
+
+    private final String albumId = "albumId";
 
     public LoadingListTask(int type, Context context){
         this.type = type;
@@ -51,7 +64,52 @@ public class LoadingListTask extends AsyncTask<Void, Void, Void> {
         return null;
     }
 
-    public ArrayList<HashMap<String, String>> files2Details(ArrayList<String> files){
+    public ArrayList<HashMap<String, String>> getSongFromProvider(){
+        ArrayList<HashMap<String, String>> songs = new ArrayList<>();
+        ContentResolver resolver = context.getContentResolver();
+        try {
+            Cursor cursor = resolver.query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, null, null, null,
+                    MediaStore.Audio.Media.DEFAULT_SORT_ORDER);
+            HashMap<String, String> item;
+            while (cursor.moveToNext()){
+                item = new HashMap<>();
+                item.put(songId, cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media._ID)));
+                item.put(songName, cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DISPLAY_NAME)));
+                item.put(artistName, cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST)));
+                item.put(albumName, cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM)));
+                item.put(duration, cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DURATION)));
+                item.put(path, cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA)));
 
+                songs.add(item);
+            }
+            return songs;
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    public ArrayList<HashMap<String, String>> getAlbumFromProvider(){
+        ArrayList<HashMap<String, String>> albums = new ArrayList<>();
+        ContentResolver resolver = context.getContentResolver();
+        try {
+            Cursor cursor = resolver.query(MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI, null, null, null,
+                    MediaStore.Audio.Albums.DEFAULT_SORT_ORDER);
+            HashMap<String, String> item;
+            while (cursor.moveToNext()){
+                item = new HashMap<>();
+                item.put(albumId, cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Albums.ALBUM_ID)));
+                item.put(albumName, cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Albums.ALBUM)));
+                item.put(artistName, cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Albums.ARTIST)));
+                item.put(songNumber, cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media._COUNT)));
+
+                albums.add(item);
+            }
+            return albums;
+        }catch (Exception e){
+            e.printStackTrace();
+            return albums;
+        }
     }
 }
